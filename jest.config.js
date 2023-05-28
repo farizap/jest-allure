093a71c033a17fcfbba3df6ec72b995a0b9af5ce
Fariz Apriyanto <fapriyanto@alterra.id>
2023-05-29 05:13:12 +0700

    first commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    collectCoverage: true,
+    reporters: ["default", "jest-allure"],
+    setupFilesAfterEnv: ["jest-allure/dist/setup"]
+};
